8521d8b45b260a4ce12c08315b8886f303d24a3d
Agas Pratama <165492024+tsukihimp4@users.noreply.github.com>
2024-10-13 15:50:17 +0700

    penambahan tugas dan jawaban

--- chapter-1/js/tugas-ch1.js
+++ chapter-1/js/tugas-ch1.js
@@ -4,3 +4,19 @@
 // 2.menentukan hari dari tanggal yang ada saat ini di pc
 
 // Jawab
+
+// Tugas nomer 1
+
+let saldoAwal = Number(prompt("Masukkan saldo awal:"));
+let saldoAkhir = Number(prompt("Masukkan saldo akhir:"));
+
+const totalSaldo = saldoAwal + saldoAkhir
+console.log("Saldo akhirmu adalah " + totalSaldo);
+
+// Tugas nomer 2
+
+const namaHari = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', "Jum'at", 'Sabtu'];
+let indeksHari = new Date().getDay();
+let hari = namaHari[indeksHari];
+
+console.log("Hari ini adalah " + hari);
