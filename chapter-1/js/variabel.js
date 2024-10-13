c983ef1ff00b7e7a8834cc1a7ee478f82620e78e
Agas Pratama <165492024+tsukihimp4@users.noreply.github.com>
2024-10-13 13:46:05 +0700

    Add files via upload

---
+++ chapter-1/js/variabel.js
@@ -0,0 +1,21 @@
+// Variable Assignment
+// untuk membuat sebuah variabel di javascript ada 3 yaitu
+
+// kedua variabel ini nilai nya bisa di ubah
+// var usia = 30
+// let usia = 30
+
+// Nilai variabel yang tidak bisa di ubah
+
+// Contoh penggunaan const
+// const usia = 40
+// usia = 34
+// Maka di console akan terjadi error Uncaught TypeError: invalid assignment to const 'usia'
+// console.log('Usiamu adalah ' + usia);
+
+// Penggunaan Var
+
+// var usia = 40
+// usia = 45
+// Maka nilai usia akan berubah menjadi 45
+// console.log("Usiamu adalah " + usia)
