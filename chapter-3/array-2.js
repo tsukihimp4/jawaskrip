1e89a4f9c11dfdbb79252fe48947926522b06722
Agas Pratama <165492024+tsukihimp4@users.noreply.github.com>
2024-10-15 21:13:36 +0700

    Add files via upload

---
+++ chapter-3/array-2.js
@@ -0,0 +1,14 @@
+// Buah-Buahan
+
+const arraySaya = [];
+
+arraySaya['alpukat'] = '🥑'
+arraySaya['jeruk'] = '🍊'
+
+// Jika hanya seperti ini, ini tidak akan terpanggil dalam konsol
+// console.log(arraySaya[0])
+
+// untuk mendapatkan hasil harus seperti ini
+console.log(arraySaya['jeruk']);
+
+// Kopi-kopian dalam sebuah array
